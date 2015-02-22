c062c9cb38608cb5db811fa4d48640e4836734a9
James Andrews <jandrews@Jamess-MacBook-Pro.local>
2015-02-22 18:26:31 +0900

    added JWT authentication modules

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,4 +1,4 @@
-module.exports = function(app, db)
+module.exports = function(app, db, jwt, jwtSecret)
 {
   var password = require('../../lib/password');
 
@@ -7,5 +7,30 @@ module.exports = function(app, db)
     var user = db.User;
     console.log(password.crypt("Test12345678910"));
     res.send('Hello World!')
+  });
+
+  app.get('/api/user/authenticate', function (req, res)
+  {
+
+    // Perform authentication here.
+
+    token = jwt.sign({
+      username: "thenetimp2000@yahoo.com"
+    }, jwtSecret);
+
+    res.send({token: token});
+
+  });
+
+  app.get('/api/user/profile', function (req, res)
+  {
+
+    profile = {
+      "firstName": "John",
+      "lastName": "Doe"
+    }
+
+    res.send(profile);
+
   });
 }
--- server.js
+++ server.js
@@ -2,31 +2,34 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var db = require('./models');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
+// Store this elsewhere later.
+var jwtSecret = "asdasfasdfawefasdfawefas";
+
+app.use(expressJwt({secret: jwtSecret}).unless({path: ['/api/user/authenticate', '/api/user/create']}));
 
 // Bootstrap controllers
 var controllersPath = __dirname + '/controllers';
-console.log(controllersPath);
-
 var controllerFiles = fs.readdirSync(controllersPath);
-console.log(controllerFiles);
-
 
 controllerFiles.forEach(function(file){
-  require(controllersPath+'/'+file)(app, db)
+  require(controllersPath+'/'+file)(app, db, jwt, jwtSecret)
 })
 
 
 app.get('/', function (req, res)
 {
-
   var user = db.User;
-  // console.log(user);
   console.log(password.crypt("Test12345678910"));
-
-
   res.send('Hello World!')
 });
 
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 var server = app.listen(3000, function () {
 
